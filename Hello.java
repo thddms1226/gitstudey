e803eada1e9e91e89146438daad009e0eea5efb4
thddms1226 <sek92@naver.com>
2020-06-01 15:25:32 +0900

    helloName()

--- Hello.java
+++ Hello.java
@@ -5,10 +5,16 @@ public class Hello {
 	public static void main(String[] args) {
 		
 		hello();		
+		helloName("µ¿¿ø");
+		helloName("kakao");
 	}
 	
 	public static void hello() {
-		System.out.println("Hello~");
+		System.out.println("Hello~ ");
+	}
+	
+	public static void helloName(String name) {
+		System.out.println("Hello~ " + name);
 	}
 	
 }
