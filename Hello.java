2ae50a9e250b81b391d47ca2efcba5c63f2babec
thddms1226 <sek92@naver.com>
2020-06-01 14:14:43 +0900

    hello()

---
+++ Hello.java
@@ -0,0 +1,14 @@
+package gittest;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		
+		hello();		
+	}
+	
+	public static void hello() {
+		System.out.println("Hello~");
+	}
+	
+}
